b0dbb94a30d123f30d556cc64b34351aea0f3fb1
trebman95 <trebman91@hotmail.com>
2024-12-09 22:31:54 -0500

    updated

--- src/org/blackplanet/automation/tb/utilities/ReadPropertiesFile.java
+++ src/org/blackplanet/automation/tb/utilities/ReadPropertiesFile.java
@@ -6,27 +6,29 @@ import java.util.Properties;
 
 public class ReadPropertiesFile {
 	
-	public static void config() throws Exception{
+	public static void config(String key) throws Exception{
 		FileReader fr = new FileReader("C:\\Users\\trebm\\eclipse-workspace\\Nov2024\\TestData\\config.properties");
 		Properties prop = new Properties();
 		prop.load(fr);
-		System.out.println(prop.get("BrowserName"));
-		System.out.println(prop.get("ApplicationURL"));
-		//System.out.println(prop.get(key));
+		
+		System.out.println(prop.get(key));
 	}
 	
 	public static void element(String key) throws Exception{
 		FileReader fr = new FileReader("C:\\Users\\trebm\\eclipse-workspace\\Nov2024\\TestData\\element.properties");
 		Properties prop = new Properties();
 		prop.load(fr);
-		System.out.println(prop.get("login_username_name"));
-		System.out.println(prop.get("login_password_name"));
-		System.out.println(prop.get("login_loginBttn_css"));
+		
+		System.out.println(prop.get(key));
 	}
 	
 	public static void main(String[] args) throws Exception {
-		config();
-		//element();
+		config("BrowserName");
+		config("ApplicationURL");
+		
+		element("login_username_name");
+		element("login_password_name");
+		element("login_loginBttn_css");	
 	}
 	
 	
